1b41a2c04b9d5d73013ef7a2f60ae3e6c0e76e79
xiangqh <bourneAc>
2019-10-29 01:20:13 +0800

    update

---
+++ build/precache-manifest.9dc409a7cb07c593b53b96c12d883685.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b179e129f35a30f2fd2f3d5062559a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc89594b464b458f10a5",
+    "url": "/static/css/2.4c0b0872.chunk.css"
+  },
+  {
+    "revision": "e6a7fdb7cad1e227edd4",
+    "url": "/static/css/main.faf4fef6.chunk.css"
+  },
+  {
+    "revision": "cc89594b464b458f10a5",
+    "url": "/static/js/2.d768ce27.chunk.js"
+  },
+  {
+    "revision": "e6a7fdb7cad1e227edd4",
+    "url": "/static/js/main.4ceeb1fa.chunk.js"
+  },
+  {
+    "revision": "62c53d82b03a2579cacd",
+    "url": "/static/js/runtime-main.bb66bf3d.js"
+  },
+  {
+    "revision": "0462fe9183beb1e7232b72664a27a163",
+    "url": "/static/media/address.0462fe91.jpeg"
+  },
+  {
+    "revision": "a9ab82a2f7a2957101b6686790fb4a20",
+    "url": "/static/media/wc.a9ab82a2.jpeg"
+  }
+]);
